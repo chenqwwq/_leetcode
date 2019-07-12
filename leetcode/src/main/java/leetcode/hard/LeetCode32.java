85fbdd45df6af6c28263d77db9af3d406d544e4f
bingxin.chen <chenbxxx@gmail.com>
2019-07-12 18:02:44 +0800

    32 just started

--- leetcode/src/main/java/leetcode/hard/LeetCode32.java
+++ leetcode/src/main/java/leetcode/hard/LeetCode32.java
@@ -3,24 +3,25 @@ package leetcode.hard;
 import java.util.Stack;
 
 /**
+ *
  * 32. Longest Valid Parentheses
  * Hard
- * <p>
+ *
  * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
- * <p>
+ *
  * Example 1:
- * <p>
+ *
  * Input: "(()"* Output: 2
  * Explanation: The longest valid parentheses substring is "()"
  * Example 2:
- * <p>
+ *
  * Input: ")()())"
  * Output: 4
  * Explanation: The longest valid parentheses substring is "()()"
- * <p>
+ *
  * 第一眼看到以为是堆栈,但是提示说是DP.
  * 完全想不到公式,用暴力写一遍试试
- * <p>
+ *
  * 刚动手写,突然发现说的是子串
  *
  * @author bingxin.chen
