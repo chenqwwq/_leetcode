0f9ac5f8faed19914726f7e44272f0feb76cb0a8
CheNbXxx <chenbxxx@gmail.com>
2020-03-15 09:40:54 +0800

    1. add 7 daily

---
+++ src/main/java/top/chenbxxx/daily/$0008.java
@@ -0,0 +1,51 @@
+package top.chenbxxx.daily;
+
+/**
+ * 322. 零钱兑换
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * 示例 2:
+ * <p>
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ * @author chen
+ * @date 2020/3/8 上午8:37
+ */
+public class $0008 {
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount + 1];
+            // dp[i]表示i元时的最小硬币数目
+            // dp[i]就等于Math.min(for(q:coins.length) ==> dp[i - q] + 1)
+            dp[0] = 0;
+
+            // 对应的硬币面值大小的都为1
+            for (int i : coins) {
+                if (i <= amount) {
+                    dp[i] = 1;
+                }
+            }
+
+            for (int i = 1; i <= amount; i++) {
+                int temp = Integer.MAX_VALUE - 1;
+                for (int c : coins) {
+                    if (i - c < 0) {
+                        continue;
+                    }
+                    temp = Math.min(temp, dp[i - c] + 1);
+                }
+                dp[i] = temp;
+            }
+
+            return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
+        }
+    }
+}
---
+++ src/main/java/top/chenbxxx/daily/$0010.java
@@ -0,0 +1,57 @@
+package top.chenbxxx.daily;
+
+/**
+ * 543. 二叉树的直径
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过根结点。
+ * <p>
+ * 示例 :
+ * 给定二叉树
+ * <p>
+ * 1
+ * / \
+ * 2   3
+ * / \
+ * 4   5
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ * <p>
+ * 注意：两结点之间的路径长度是以它们之间边的数目表示。
+ *
+ * @author chen
+ * @date 2020/3/10 下午1:04
+ */
+public class $0010 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        private int max = 0;
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            dfs(root);
+            return max;
+        }
+
+        /**
+         * 返回的是该节点最深的子树深度
+         */
+        private int dfs(TreeNode root) {
+            // 当前节点为空，深度即为0
+            if (root == null) {
+                return 0;
+            }
+
+            // 返回右边的最大深度
+            final int r = dfs(root.right);
+            final int l = dfs(root.left);
+            max = Math.max(max, r + l);
+            return Math.max(r, l) + 1;
+        }
+    }
+}
