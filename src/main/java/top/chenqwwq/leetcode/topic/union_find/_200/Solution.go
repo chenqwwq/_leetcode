50163f7a3ea163012bb641287be72f112707c78f
chenqwwq <chenbxxx@gmail.com>
2020-12-31 09:38:04 +0800

    daily & dp

--- src/main/java/top/chenqwwq/leetcode/topic/union_find/_200/Solution.go
+++ src/main/java/top/chenqwwq/leetcode/topic/union_find/_200/Solution.go
@@ -38,6 +38,7 @@ grid[i][j] 的值为 '0' 或 '1'
 
 type UnionFind struct {
 	tree []int
+	rank []int
 }
 
 func NewUnionFind(grid [][]byte) (*UnionFind, int, int) {
@@ -46,7 +47,7 @@ func NewUnionFind(grid [][]byte) (*UnionFind, int, int) {
 		return nil, -1, -1
 	}
 	p := len(grid[0])
-	tree := make([]int, q*p)
+	tree, rank := make([]int, q*p), make([]int, q*p)
 	// 一开始就在同一棵树上
 	for i := 0; i < q; i++ {
 		for j := 0; j < p; j++ {
@@ -56,20 +57,42 @@ func NewUnionFind(grid [][]byte) (*UnionFind, int, int) {
 			} else {
 				tree[curr] = curr
 			}
+			rank[curr] = 1
 		}
 	}
-	return &UnionFind{tree: tree}, q, p
+	return &UnionFind{tree: tree, rank: rank}, q, p
 }
 
-func (d *UnionFind) Find(i int) int {
-	if d.tree[i] != i {
-		d.tree[i] = d.Find(d.tree[i])
+func (find *UnionFind) Find(i int) int {
+	if find.tree[i] != i {
+		find.tree[i] = find.Find(find.tree[i])
 	}
-	return d.tree[i]
+	return find.tree[i]
 }
 
-func (d *UnionFind) Union(i, j int) {
-	d.tree[d.Find(i)] = d.Find(j)
+func (find *UnionFind) Union(i, j int) {
+	q, p := find.Find(i), find.Find(j)
+	if q == p {
+		return
+	}
+	if find.rank[q] > find.rank[p] {
+		find.tree[p] = q
+	} else {
+		if find.rank[q] == find.rank[p] {
+			find.rank[p] ++
+		}
+		find.tree[q] = p
+	}
+}
+
+func (find *UnionFind) getCnt() int {
+	hash := make(map[int]interface{})
+	for i := 0; i < len(find.tree); i++ {
+		if find.tree[i] != -1 {
+			hash[find.Find(i)] = 'A'
+		}
+	}
+	return len(hash)
 }
 
 func numIslands(grid [][]byte) int {
@@ -87,19 +110,10 @@ func numIslands(grid [][]byte) int {
 			grid[i][j] = '0'
 
 			curr := i*p + j
-			j1 := j - 1
-			if j1 >= 0 && grid[i][j1] == '1' {
-				find.Union(curr-1, curr)
-			}
 
 			j2 := j + 1
 			if j2 < p && grid[i][j2] == '1' {
 				find.Union(curr+1, curr)
-			}
-
-			i1 := i - 1
-			if i1 >= 0 && grid[i1][j] == '1' {
-				find.Union(curr-q, curr)
 			}
 
 			i2 := i + 1
@@ -110,12 +124,5 @@ func numIslands(grid [][]byte) int {
 		}
 	}
 
-	hash := make(map[int]interface{})
-	for i := 0; i < len(find.tree); i++ {
-		if find.tree[i] != -1 {
-			hash[find.tree[i]] = "A"
-		}
-	}
-
-	return len(hash)
+	return find.getCnt()
 }
