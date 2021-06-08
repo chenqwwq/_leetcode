a4a49723d8c03f7f065ce6cdc28e979cd93cbb78
chenqwwq <chenbxxx@gmail.com>
2021-06-08 18:03:54 +0800

    daily

---
+++ src/main/java/top/chenqwwq/leetcode/topic/union_find/_128/Solution.java
@@ -0,0 +1,5 @@
+package top.chenqwwq.leetcode.topic.union_find._128;/**
+ *  @author chen
+ *  @date   2021-06-08
+ **/public class Solution {
+}
---
+++ src/main/java/top/chenqwwq/leetcode/topic/union_find/_128/SolutionTest.java
@@ -0,0 +1,14 @@
+package top.chenqwwq.leetcode.topic.union_find._128;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *  @author chen
+ *  @date   2021-06-08
+ **/class SolutionTest {
+
+	@Test
+	void longestConsecutive() {
+	}
+}
